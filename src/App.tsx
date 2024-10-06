f2f876fc5795fe7a7e149468524ac34653962e99
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-10-06 20:30:19 +0300

    Added: main layout

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+// import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import Home from './routes/Home'
+import GettingReady from './routes/GettingReady'
+import Quiz from './routes/Quiz'
+import Result from './routes/Result'
+
+import './App.css'
+
+const App = () => {
+  const [quizPreferences, setQuizPreferences] = useState({
+    category: 0,
+    difficulty: 0,
+    type: 0,
+  })
+
+  // const [quizAnswers,] = useState({
+  //   question1: '',
+  //   question2: '',
+  //   question3: '',
+  //   question4: '',
+  // })
+
+  return (
+    <BrowserRouter>
+      <main className='w-full h-screen flex flex-col justify-center bg-custom-bg font text-custom-color font-karla-font'>
+        <img className="fixed right-0 top-0" src='/tr-shape.png' alt="Shape Top" />
+        <section className='flex flex-col self-center w-fit h-fit justify-center max--width'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/getting-ready' element={<GettingReady setFormData={setQuizPreferences} />} />
+            <Route path='/quiz' element={<Quiz formData={quizPreferences} />} />
+            <Route path='/result' element={<Result />} />
+          </Routes>
+        </section>
+        <img className="fixed left-0 bottom-0" src='/bl-shape.png' alt="Shape Bottom" />
+      </main>
+    </BrowserRouter>
+  )
+}
+
+export default App
