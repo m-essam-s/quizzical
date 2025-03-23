3d886eadf1e789e9eef6f70e4f0f6470d5c05a7c
m-essam-s <sir.m.essam.g@gmail.com>
2025-03-23 23:08:26 +0200

    Update App component to handle quiz preferences and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 import Home from './routes/Home'
@@ -11,20 +10,28 @@ const App = () => {
   const [quizPreferences, setQuizPreferences] = useState({
     category: 0,
     difficulty: 0,
+    amount: 0,
     type: 0,
   })
-
-
 
   return (
     <BrowserRouter>
       <img className="fixed right-0 top-0 z-0" src='/tr-shape.png' alt="Shape Top" />
-      <main className='w-full  flex align-middle justify-center  font text-custom-color font-karla-font py-6 px-3 z-10 scroll-my-10'>
-        <section className='flex flex-col self-center w-fit h-auto justify-center max--width z-10 '>
+      <main className='w-full h-screen flex flex-col justify-center items-center text-custom-color font-karla-font z-10 py-2'>
+        <section className='flex flex-col justify-center z-10 overflow-y-auto'>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/getting-ready' element={<GettingReady setFormData={setQuizPreferences} />} />
-            <Route path='/quiz' element={<Quiz formData={quizPreferences} />} />
+            <Route
+              path='/'
+              element={<Home />}
+            />
+            <Route
+              path='/getting-ready'
+              element={<GettingReady setFormData={setQuizPreferences} />}
+            />
+            <Route
+              path='/quiz'
+              element={<Quiz quizPreferences={quizPreferences} />}
+            />
           </Routes>
         </section>
       </main>
